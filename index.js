13a284205d40dafc4e11abc380ebdf462a1a73ca
adamdbdev <41781341+adamdbdev@users.noreply.github.com>
2018-08-10 11:49:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var http = require('http');
 
+import { Form, Text } from 'react-form';
+
+const validate = value => ({
+  error: !value || !/Hello World/.test(value) ? "Input must contain 'Hello World'" : null,
+  warning: !value || !/^Hello World$/.test(value) ? "Input should equal just 'Hello World'" : null,
+  success: value && /Hello World/.test(value) ? "Thanks for entering 'Hello World'!" : null
+})
+
+<Form>
+  {formApi => (
+    <form onSubmit={formApi.submitForm} id="form1" className="mb-4">
+      <label htmlFor="hello">Hello World</label>
+      <Text field="hello" id="hello" validate={validate} />
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  )}
+</Form>
+
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
