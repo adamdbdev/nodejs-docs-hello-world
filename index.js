1ac43e419c061a4927d770208bfda5bf82d095f0
adamdbdev <41781341+adamdbdev@users.noreply.github.com>
2018-08-10 11:57:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var http = require('http');
 
 import { Form, Text } from 'informed';
+
+<Form id="simple-form">
+    <label htmlFor="name-field">First name:</label>
+    <Text field="name" id="name-field" />
+    <button type="submit">
+      Submit
+    </button></Form>
  
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello, you've got mail!");
-   <Form id="simple-form">
-    <label htmlFor="name-field">First name:</label>
-    <Text field="name" id="name-field" />
-    <button type="submit">
-      Submit
-    </button>
-   </Form>
-
 });
 
 var port = process.env.PORT || 1337;
