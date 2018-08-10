8bdd6e4e8426866e6d715330d0916666a23978e5
adamdbdev <41781341+adamdbdev@users.noreply.github.com>
2018-08-10 12:17:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,24 @@ var server = http.createServer(function(request, response) {
 
 var port = process.env.PORT || 1337;
 server.listen(port);
-
+import React from 'react'
+import {Form, Field} from 'simple-react-form'
+import DatePicker from './myFields/DatePicker'
+import Text from './myFields/Text'
+ 
+class PostsCreate extends React.Component {
+  state = {}
+ 
+  render() {
+    return (
+      <div>
+        <Form state={this.state} onChange={state => this.setState(state)}>
+          <Field fieldName="name" label="Name" type={Text} />
+          <Field fieldName="date" label="A Date" type={DatePicker} />
+        </Form>
+        <p>My name is {this.state.name}</p>
+      </div>
+    )
+  }
+}
 console.log("Server running at http://localhost:%d", port);
